0b658c62ab9639060bc5eeb05f137abfb461bf92
Matthew Kim <matthew.kim64@gmail.com>
2018-05-03 17:41:56 -0700

    im doing stuff idk lololol
    pray it works lmao

---
+++ New folder/src/ChordMessageInterface.java
@@ -0,0 +1,34 @@
+import java.rmi.*;
+import java.io.*;
+import java.util.*;
+
+public interface ChordMessageInterface extends Remote
+{
+    public ChordMessageInterface getPredecessor()  throws RemoteException;
+    ChordMessageInterface locateSuccessor(long key) throws RemoteException;
+    ChordMessageInterface closestPrecedingNode(long key) throws RemoteException;
+    public void joinRing(String Ip, int port)  throws RemoteException;
+    public void notify(ChordMessageInterface j) throws RemoteException;
+    public boolean isAlive() throws RemoteException;
+    public long getId() throws RemoteException;
+    
+    public void emitMap(Long key, String value) throws RemoteException;
+    public void emitReduce(Long key, String value) throws RemoteException;
+    
+    public void put(long guidObject, InputStream inputStream) throws IOException, RemoteException;
+    public InputStream get(long guidObject) throws IOException, RemoteException;
+    public void delete(long guidObject) throws IOException, RemoteException;
+
+    public void setWorkingPeer(Long page) throws RemoteException;
+    public void completePeer(Long page, Long n) throws RemoteException;
+    public Boolean isPhaseCompleted() throws RemoteException;
+    
+    public void reduceContext(Long source, MapReduceInterface reducer, ChordMessageInterface context) throws RemoteException;
+    public void mapContext(Long page, MapReduceInterface mapper, ChordMessageInterface context) throws RemoteException;
+
+    public void map(Long key, String value, ChordMessageInterface context) throws IOException;
+    public void reduce(Long key, List< String > value, ChordMessageInterface context) throws IOException;
+    
+    public Map<Long, String> getPredecessorReduce() throws RemoteException;
+    public Map<Long, String> getSuccessorReduce() throws RemoteException;
+}
